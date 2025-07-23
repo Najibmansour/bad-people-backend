3a819bfb1d759b032bef5a018070f03674dc3fb5
Najib Mansour BitBucket <najib.mansour@axentech.io>
2025-07-23 19:20:40 +0300

    added user sto

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, Matches, Max, Min } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  username: string;
+
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    {
+      message:
+        'Password must be at least 8 characters long, contain uppercase, lowercase, number, and special character',
+    },
+  )
+  password: string;
+}
