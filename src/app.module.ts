ca07c6d92661ca4bdbebf72fc8c80e6b0ee2679d
Najib Mansour BitBucket <najib.mansour@axentech.io>
2025-07-23 18:48:39 +0300

    added TypeOrm  and dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres', // or 'mysql', 'sqlite'
+        host: config.get('DB_HOST'),
+        port: 5432,
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASS'),
+        database: config.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // Disable in production!
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
